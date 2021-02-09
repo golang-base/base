738a8e799842a46ba0b361d2142358120ebe3624
zz-link <appx@outlook.com>
2021-02-10 02:31:40 +0800

    up

---
+++ config.go
@@ -0,0 +1,45 @@
+package base
+
+import (
+"fmt"
+"github.com/spf13/viper"
+"log"
+)
+
+var Config _Config
+
+func InitConfig() {
+	viper.SetConfigName("dev")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("./conf/")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("read config failed: %v", err)
+	}
+
+	viper.Unmarshal(&Config)
+
+	fmt.Println(Config.MySQL)
+}
+
+type _Config struct {
+	AppName  string `mapstructure:"app_name"`
+	LogLevel string `mapstructure:"log_Level"`
+
+	MySQL _MySQLConfig
+	Redis _RedisConfig
+}
+
+type _MySQLConfig struct {
+	IP       string
+	Port     int
+	User     string
+	Password string
+	Database string
+}
+
+type _RedisConfig struct {
+	IP   string
+	Port int
+}
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/golang-base/base
+
+go 1.15
+
+require github.com/spf13/viper v1.7.1
